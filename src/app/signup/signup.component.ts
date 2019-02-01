fe8dee2d94ef4e674218091a07928734012d44e7
Amar1991 <jhkumar92@gmail.com>
2019-02-01 12:16:02 +0000

    test phase 2

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -11,7 +11,7 @@ export class SignupComponent implements OnInit {
   showSuccess:boolean;
   serverError: string;
 
-  constructor(private userService:UserService) {
+  constructor(public userService:UserService) {
     
    }
 
