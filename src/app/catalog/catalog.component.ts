cc307aa2e6771136f40eaaa59c9c315a46775a50
Amar1991 <jhkumar92@gmail.com>
2019-02-01 17:58:25 +0000

    test phase 7

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -22,7 +22,7 @@ export class CatalogComponent implements OnInit {
 
   ngOnInit() {
     
-    this.productService.getProductList().subscribe(res=>{this.productlist=res.json()});
+    this.productService.getProductList().subscribe(res=>{console.log(res)});
           }
 
  
